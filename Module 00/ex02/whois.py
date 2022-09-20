cf70b9522528edba1e8b2c6489e68951cc81454c
Nywgates <aballea.louis@gmail.com>
2022-09-20 11:34:13 +0200

    ex02 done

---
+++ Module 00/ex02/whois.py
@@ -0,0 +1,17 @@
+import sys
+
+
+def main(args):
+    if (len(args) != 2):
+        print("Usage: enter one arguments.")
+    elif (args[1].isdigit() == False):
+        print("Usage: arguments must be interger.")
+    elif (int(args[1]) == 0):
+        print("I'm Zero.")
+    elif (int(args[1]) % 2 == 1):
+        print("I'm Odd.")
+    else:
+        print("I'm Even.")
+
+
+main(sys.argv)
