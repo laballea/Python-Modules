baf9223b15c79c23009d049ee37b7c1b4514941c
Nywgates <aballea.louis@gmail.com>
2022-09-20 12:35:51 +0200

    ex02 done

--- Module 00/ex02/whois.py
+++ Module 00/ex02/whois.py
@@ -5,7 +5,7 @@ def main(args):
     if (len(args) != 2):
         print("Usage: enter one arguments.")
     elif (args[1].isdigit() == False):
-        print("Usage: arguments must be interger.")
+        print("Usage: arguments must be integer.")
     elif (int(args[1]) == 0):
         print("I'm Zero.")
     elif (int(args[1]) % 2 == 1):
