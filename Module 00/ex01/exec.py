faa6c4c004200b160ab3c1117ef95feae6725e7c
Nywgates <aballea.louis@gmail.com>
2022-09-24 16:08:38 +0200

    ex01 done

--- Module 00/ex01/exec.py
+++ Module 00/ex01/exec.py
@@ -5,7 +5,6 @@ def main(args):
     if (len(args) == 1):
         print("Usage: enter arguments.")
     else:
-        string = concatenateArgs(args)
         print(' '.join(args[1:]).swapcase()[::-1])
 
 
