251ca7693ce889794b954551af4a645c645f257b
Nywgates <aballea.louis@gmail.com>
2022-10-03 09:37:39 +0200

    ex00 done

---
+++ Module 02/ex00/ft_map.py
@@ -0,0 +1,23 @@
+from typing import Iterable
+
+
+def ft_map(function_to_apply, iterable):
+	"""Map the function to all elements of the iterable.
+	Args:
+	function_to_apply: a function taking an iterable.
+	iterable: an iterable object (list, tuple, iterator).
+	Return:
+	An iterable.
+	None if the iterable can not be used by the function.
+	"""
+	if (not isinstance(iterable, Iterable)):
+		raise TypeError("{} is not iterable".format(type(iterable).__name__))
+	try:
+		for el in iterable:
+			yield function_to_apply(el)
+	except:
+		return None
+
+numbers = [10, 15, 21, 33, 42, 55]
+print(list(map(lambda x: x * 2 + 3, numbers)))
+print(list(ft_map(lambda x: x * 2 + 3, numbers)))
---
+++ Module 02/ex00/ft_reduce.py
@@ -0,0 +1,44 @@
+from functools import reduce
+from typing import Iterable
+
+
+def ft_reduce(function_to_apply, iterable):
+	"""Apply function of two arguments cumulatively.
+	Args:
+	function_to_apply: a function taking an iterable.
+	iterable: an iterable object (list, tuple, iterator).
+	Return:
+	A value, of same type of elements in the iterable parameter.
+	None if the iterable can not be used by the function.
+	"""
+	if (not isinstance(iterable, Iterable)):
+		raise TypeError("{} is not iterable".format(type(iterable).__name__))
+	try:
+		value = iterable[0]
+		for el in iterable[1:]:
+			value = function_to_apply(value, el)
+		return value
+	except:
+		return None
+
+import operator
+ 
+# initializing list
+lis = [1, 3, 5, 6, 2, ]
+ 
+# using reduce to compute sum of list
+# using operator functions
+print("The sum of the list elements is : ", end="")
+print(reduce(operator.add, lis))
+print(ft_reduce(operator.add, lis))
+ 
+# using reduce to compute product
+# using operator functions
+print("The product of list elements is : ", end="")
+print(reduce(operator.mul, lis))
+print(ft_reduce(operator.mul, lis))
+ 
+# using reduce to concatenate string
+print("The concatenated product is : ", end="")
+print(reduce(operator.add, ["geeks", "for", "geeks"]))
+print(ft_reduce(operator.add, ["geeks", "for", "geeks"]))
