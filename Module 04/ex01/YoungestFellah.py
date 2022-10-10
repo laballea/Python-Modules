485a421503b8e5336fd3323a6d0c2b6906cdc084
Nywgates <aballea.louis@gmail.com>
2022-10-10 10:45:08 +0200

    ex01 update

--- Module 04/ex01/YoungestFellah.py
+++ Module 04/ex01/YoungestFellah.py
@@ -2,10 +2,9 @@ from FileLoader import FileLoader
 import pandas
 
 def	youngest_fellah(df:pandas.DataFrame, year:int):
-	print(df.columns)
-	man = data["Sex"]=="M"
-	women = data["Sex"]=="F"
-	year = data["Year"]==year
+	man = df["Sex"]=="M"
+	women = df["Sex"]=="F"
+	year = df["Year"]==year
 	return {
 		"f":df.where(year).where(women)['Age'].min(),
 		"m":df.where(year).where(man)['Age'].min()
