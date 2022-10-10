20263e87845ccbf894eb06ce53a391d3981f7701
Nywgates <aballea.louis@gmail.com>
2022-10-10 13:57:12 +0200

    ex04 done

---
+++ Module 04/ex04/SpatioTemporalData.py
@@ -0,0 +1,36 @@
+from numpy import array
+import pandas
+
+
+class SpatioTemporalData():
+	def __init__(self, df:pandas.DataFrame):
+		self.df = df
+
+	def when(self, location:str):
+		"""
+			takes a location as an argument and returns a list containing the
+			years where games were held in the given location
+		"""
+		loc_cond = self.df["City"] == location
+		result = self.df.where(loc_cond).drop_duplicates(subset=['Year'])['Year']
+
+		return array(result.dropna())
+	def where(self, date:int):
+		"""
+			takes a date as an argument and returns the location where the
+			Olympics took place in the given year
+		"""
+		date_cond = self.df["Year"] == date
+		result = self.df.where(date_cond).drop_duplicates(subset=['City'])['City']
+
+		return array(result.dropna())
+
+from FileLoader import FileLoader
+loader = FileLoader()
+data = loader.load("../ressource/athlete_events.csv")
+
+sp = SpatioTemporalData(data)
+print(sp.where(1896))
+print(sp.where(2016))
+print(sp.when("Athina"))
+print(sp.when("Paris"))
