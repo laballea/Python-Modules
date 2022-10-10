108d889cc775e8dd143b65e5bcbdd599a3e2b274
Nywgates <aballea.louis@gmail.com>
2022-10-10 15:34:30 +0200

    ex06 done

---
+++ Module 04/ex06/MyPlotLib.py
@@ -0,0 +1,48 @@
+from matplotlib import pyplot as plt
+import pandas
+from FileLoader import FileLoader
+import seaborn
+
+class MyPlotLib():
+	def histogram(data:pandas.DataFrame, features:list): 
+		"""
+			plots one histogram for each numerical feature in
+			the list
+		"""
+		data[features].hist()
+		plt.show()
+
+	def density(data, features):
+		"""
+			plots the density curve of each numerical feature in
+			the list
+		"""
+		data[features].plot.density()
+		plt.show()
+
+	def pair_plot(data, features):
+		"""
+			plots a matrix of subplots (also called scatter plot
+			matrix). On each subplot shows a scatter plot of one numerical variable against
+			another one. The main diagonal of this matrix shows simple histograms
+		"""
+		seaborn.pairplot(data[features])
+		plt.show()
+
+	def box_plot(data, features):
+		"""
+			displays a box plot for each numerical variable in the
+			dataset
+		"""
+		seaborn.boxplot(data[features])
+		plt.show()
+
+
+loader = FileLoader()
+data = loader.load("../ressource/athlete_events.csv")
+MyPlotLib.histogram(data, ["Weight", "Height"])
+MyPlotLib.density(data, ["Weight", "Height"])
+MyPlotLib.pair_plot(data, ["Weight", "Height"])
+MyPlotLib.box_plot(data, ["Weight", "Height"])
+
+
