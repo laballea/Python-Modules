5aa39f06e88b1263086d122cc57c1d9f95657701
Nywgates <aballea.louis@gmail.com>
2022-10-10 11:11:28 +0200

    ex03 done

---
+++ Module 04/ex03/HowManyMedals.py
@@ -0,0 +1,21 @@
+import pandas
+
+
+def how_many_medals(df:pandas.DataFrame, name:str):
+	name_cond = df["Name"]==name
+	athlete = df[name_cond]
+	result  = {}
+	for year in athlete['Year']:
+		result[year] = {
+			"G":athlete[(athlete['Year']==year) & (athlete['Medal'] == "Gold")].shape[0],
+			"S":athlete[(athlete['Year']==year) & (athlete['Medal'] == "Silver")].shape[0],
+			"B":athlete[(athlete['Year']==year) & (athlete['Medal'] == "Bronze")].shape[0]
+		}
+	return result
+
+
+
+from FileLoader import FileLoader
+loader = FileLoader()
+data = loader.load("../ressource/athlete_events.csv")
+print(how_many_medals(data, "Kjetil Andr Aamodt"))
